3d74575a1527de8382563631cd2f51d3fa08e9e9
benjaminwan <benjaminwan@163.com>
2020-09-26 14:17:26 +0800

    选择图片

--- app/src/main/java/com/benjaminwan/ocr/MainActivity.kt
+++ app/src/main/java/com/benjaminwan/ocr/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.benjaminwan.ocr
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var selectedImg: Bitmap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        resultTV.movementMethod = ScrollingMovementMethod.getInstance()
+        selectBtn.setOnClickListener(this)
+        detectBtn.setOnClickListener(this)
     }
+
+    override fun onClick(view: View?) {
+        view ?: return
+        when (view.id) {
+            R.id.selectBtn -> {
+                val intent = Intent(Intent.ACTION_PICK).apply {
+                    type = "image/*"
+                }
+                startActivityForResult(
+                    intent, REQUEST_SELECT_IMAGE
+                )
+            }
+            R.id.detectBtn -> {
+
+            }
+            else -> {
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        data ?: return
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_SELECT_IMAGE) {
+            val imgUri = data.data ?: return
+            val options =
+                RequestOptions().skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE)
+            Glide.with(this).load(imgUri).apply(options).into(imageView)
+            selectedImg = decodeUri(imgUri)
+        }
+    }
+
+    companion object {
+        const val REQUEST_SELECT_IMAGE = 666
+    }
+
 }
