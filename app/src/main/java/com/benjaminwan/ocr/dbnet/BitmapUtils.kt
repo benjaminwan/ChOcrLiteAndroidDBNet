d5ad21472d8a6ae7db844e9be0f23fdca5ae2303
benjaminwan <benjaminwan@163.com>
2020-09-28 17:36:05 +0800

    改包名

--- app/src/main/java/com/benjaminwan/ocr/dbnet/BitmapUtils.kt
+++ app/src/main/java/com/benjaminwan/ocr/dbnet/BitmapUtils.kt
@@ -1,4 +1,4 @@
-package com.benjaminwan.ocr
+package com.benjaminwan.ocr.dbnet
 
 import android.content.Context
 import android.graphics.Bitmap
--- app/src/main/java/com/benjaminwan/ocr/dbnet/MainActivity.kt
+++ app/src/main/java/com/benjaminwan/ocr/dbnet/MainActivity.kt
@@ -1,14 +1,14 @@
-package com.benjaminwan.ocr
+package com.benjaminwan.ocr.dbnet
 
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.View
 import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
 import com.benjaminwan.ocrlibrary.OcrEngine
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
